97238d636f9d5456fcbba3d09893405d3efb61bd
amanaggarwal1 <i.aggarwalaman@gmail.com>
2020-03-23 17:16:23 +0530

    Added a slider to control the music volume.

--- app/src/main/java/com/example/basicaudioplayer/MainActivity.java
+++ app/src/main/java/com/example/basicaudioplayer/MainActivity.java
@@ -2,27 +2,60 @@ package com.example.basicaudioplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button playPauseButton;
+    private SeekBar volumeSeekBar;
     private MediaPlayer mediaPlayer;
+    private AudioManager audioManager;
+
+    private boolean isMusicPlaying = false;
 
     public void playMusic(View view){
-        mediaPlayer.start();
+
+        if(isMusicPlaying) mediaPlayer.pause();
+        else mediaPlayer.start();
+
+        isMusicPlaying ^= true;
     }
 
-    public void pauseMusic(View view){
-        mediaPlayer.pause();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        volumeSeekBar = findViewById(R.id.seekBar);
+        playPauseButton = findViewById(R.id.playPauseButton);
         mediaPlayer = MediaPlayer.create(this, R.raw.rainforest_music);
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        volumeSeekBar.setMax(maxVolume);
+
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, progress, 0);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
