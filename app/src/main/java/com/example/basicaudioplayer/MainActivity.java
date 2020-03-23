89e85910444d999d06c88723c58b1e0ae6ac4a2c
amanaggarwal1 <i.aggarwalaman@gmail.com>
2020-03-23 23:52:08 +0530

    Added a slider to control music playback and it updates itself 10 times every second (if the music is being played) for smoother transitions and optimised battery life.

--- app/src/main/java/com/example/basicaudioplayer/MainActivity.java
+++ app/src/main/java/com/example/basicaudioplayer/MainActivity.java
@@ -5,15 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.service.voice.VoiceInteractionSession;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button playPauseButton;
-    private SeekBar volumeSeekBar;
+    private SeekBar volumeSeekBar,  scrubSeekBar;
     private MediaPlayer mediaPlayer;
     private AudioManager audioManager;
 
@@ -33,13 +38,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        volumeSeekBar = findViewById(R.id.seekBar);
+        volumeSeekBar = findViewById(R.id.volumeseekBar);
+        scrubSeekBar = findViewById(R.id.scrubSeekBar);
         playPauseButton = findViewById(R.id.playPauseButton);
+
         mediaPlayer = MediaPlayer.create(this, R.raw.rainforest_music);
         audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
 
         int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
         volumeSeekBar.setMax(maxVolume);
+        volumeSeekBar.setProgress(currentVolume);
 
         volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -57,5 +66,34 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        scrubSeekBar.setMax(mediaPlayer.getDuration());
+
+        scrubSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mediaPlayer.seekTo(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                playMusic(seekBar);
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                playMusic(seekBar);
+            }
+        });
+
+        if (!isMusicPlaying) {
+            new Timer().scheduleAtFixedRate(new TimerTask() {
+                @Override
+                public void run() {
+                    scrubSeekBar.setProgress(mediaPlayer.getCurrentPosition());
+                }
+            }, 0, 100);
+
+        }
     }
 }
